19330fcc74f154c7391a9a0d6ad9ad730c4ec035
Konsta12121 <86122270+Konsta12121@users.noreply.github.com>
2023-06-28 19:26:02 +0300

    Update sv_proppir.lua

--- sv_proppir.lua
+++ sv_proppir.lua
@@ -1,40 +1,11 @@
 
 local ESX = exports['es_extended']:getSharedObject()
 
-lib.callback.register('palkkio', function()
+lib.callback.register('parkingMeter:reward', function(source)
     local itemAmount = math.random(500, 2000)
     exports.ox_inventory:AddItem(source, 'money', itemAmount, nil, nil, nil)
 end)
 
-lib.callback.register("onkoryostettu", function()
-    while true do
-        if ryostettu then
-            TriggerClientEvent('esx:showNotification', source, 'Tää on jo ryöstetty!', 'error', 3000)
-            SetTimeout(600000, function()
-                ryostettu = false
-            
-            end)
-        else
-                ryostettu = true
-            lib.callback("ryosto", source, false)
-           end
-   Wait(1000)
-    end
-end)
-
-
-lib.callback.register('onkopopo', function(source)
-    local poliisi = 0
-    local pelaajas = GetPlayers()
-    for i=1, #pelaajas, 1 do
-        local pelaaja = ESX.GetPlayerFromId(pelaajas[i])
-        if pelaaja ~= nil and pelaaja.job.name == 'police' then
-            poliisi = poliisi + 1
-        end
-    end
-    if poliisi >= 1 then
-        lib.callback.await('ryosto', source)
-    else
-        print("eikyttia")
-    end
+lib.callback.register('parkingMeter:getPoliceCount', function(source)
+    return #ESX.GetExtendedPlayers('job', 'police')
 end)
